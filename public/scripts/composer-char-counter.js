b085136c90ecee3ef148495d4594019d76f153a0
Kenia Balestra <kenia.balestra@gmail.com>
2022-09-14 17:53:31 +0000

    Add css files to header, navbar, newTweet. Add Char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#tweet-text').keyup(function() {
+    let charLimit = 140;
+    let charRemaining = charLimit - $(this).val().length;
+    $(".counter").text(charRemaining);
+   
+    if (charRemaining < 0) {
+      $(".counter").addClass("exceed-limit");
+    } else {
+      $(".counter").removeClass("exceed-limit");
+    }
+  });
+});
+
+
+  // --- our code goes here ---
